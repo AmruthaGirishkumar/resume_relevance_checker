2af4325b99ea43749eadc074b90d2c0b1b29de45
Amrutha Girishkumar <162736549+AmruthaGirishkumar@users.noreply.github.com>
2025-09-20 17:42:00 +0530

    Update parsing.py

--- app/src/parsing.py
+++ app/src/parsing.py
@@ -7,19 +7,16 @@ def extract_text(uploaded_file):
     Extract text from uploaded_file (txt, pdf, or docx).
     uploaded_file is a Streamlit UploadedFile object.
     """
-    # TXT
     if uploaded_file.type == "text/plain":
         return uploaded_file.getvalue().decode("utf-8")
     
-    # DOCX
     elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
         doc = docx.Document(io.BytesIO(uploaded_file.getvalue()))
         return "\n".join([para.text for para in doc.paragraphs])
     
-    # PDF
     elif uploaded_file.type == "application/pdf":
         reader = PdfReader(io.BytesIO(uploaded_file.getvalue()))
-        return "\n".join([page.extract_text() for page in reader.pages])
+        return "\n".join([page.extract_text() for page in reader.pages if page.extract_text()])
     
     else:
         return ""
